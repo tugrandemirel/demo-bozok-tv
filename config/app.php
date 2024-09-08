fe053041a1eef250ea7884d14e342fbc46f89b94
Tuğran Demirel <demireltugran66@gmail.com>
2024-09-08 18:13:31 +0300

    spati sortable kuurulumu

--- config/app.php
+++ config/app.php
@@ -196,7 +196,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
-
+        Spatie\EloquentSortable\EloquentSortableServiceProvider::class,
     ],
 
     /*
