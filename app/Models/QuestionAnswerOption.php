e0a2b6c2667e6b46af3b14f4e9dbe00e3cd77c11
Tugran Demirel <demireltugran66@gmail.com>
2024-11-22 23:35:22 +0300

    #65
    KVKK kapsamında kullanıcnın ankete katılırken alınan bilgilerin tutulacağı tabloların dizaynı

--- app/Models/QuestionAnswerOption.php
+++ app/Models/QuestionAnswerOption.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class QuestionAnswerOption extends Model
@@ -15,6 +16,10 @@ class QuestionAnswerOption extends Model
         "survey_question_id",
         "created_by_user_id",
         "answer_text",
-
     ];
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(SurveyQuestion::class);
+    }
 }
--- app/Models/SurveyUserKvkkData.php
+++ app/Models/SurveyUserKvkkData.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SurveyUserKvkkData extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "uuid",
+        "question_user_answer_id",
+        "ip_address",
+        "browser",
+        "os",
+        "location",
+    ];
+
+    public function questionUserAnswer(): BelongsTo
+    {
+        return $this->belongsTo(QuestionUserAnswer::class);
+    }
 }
